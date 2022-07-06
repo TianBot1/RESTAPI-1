c83fa6e88b6293333ea5ce1d627630ec9c591c48
Christian ID <100929366+TianBot1@users.noreply.github.com>
2022-07-06 23:02:53 +0700

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ __path = process.cwd()
 var express = require('express');
 
 // MASUKKAN/GANTI APIKEY 
-apikey = "HitomiMd"
+apikey = "HitomiMd", "Hitomi", "IzumiMd"
 // 
 
 var fetch = require('node-fetch');
